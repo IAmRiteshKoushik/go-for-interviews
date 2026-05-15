8d640cf8eabfce803acb1499681938c2ad53c546
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2026-05-15 14:23:30 +0530

    abstract factory in Go

--- pattern/abstract-factory-pattern/main.go
+++ pattern/abstract-factory-pattern/main.go
@@ -1,65 +1,21 @@
 package main
 
-import (
-	"fmt"
-)
-
-// Define an abstract factory
-type GUIFactory interface {
-	CreateButton() Button
-	CreateCheckbox() Checkbox
-}
-
-// Define interfaces for products
-type (
-	Button interface {
-		Press() string
-	}
-
-	Checkbox interface {
-		Check() string
-	}
-
-	// Implement concrete products for Windows
-	WindowsButton   struct{}
-	WindowsCheckbox struct{}
-
-	// Implement concrete products for Mac
-	MacButton   struct{}
-	MacCheckbox struct{}
-
-	// Implement factories for each platform
-	WindowsFactory struct{}
-	MacFactory     struct{}
-)
-
-func (w *WindowsButton) Press() string { return "Windows Button Pressed" }
-
-func (w *WindowsCheckbox) Check() string { return "Windows Checkbox Checked" }
-
-func (m *MacButton) Press() string { return "Mac Button Pressed" }
-
-func (m *MacCheckbox) Check() string { return "Mac Checkbox Checked" }
-
-func (w *WindowsFactory) CreateButton() Button     { return &WindowsButton{} }
-func (w *WindowsFactory) CreateCheckbox() Checkbox { return &WindowsCheckbox{} }
-
-func (m *MacFactory) CreateButton() Button     { return &MacButton{} }
-func (m *MacFactory) CreateCheckbox() Checkbox { return &MacCheckbox{} }
+import "fmt"
 
 func main() {
-	// Get a Windows factory
-	var wf GUIFactory = &WindowsFactory{}
-	button := wf.CreateButton()
-	checkbox := wf.CreateCheckbox()
 
-	fmt.Println(button.Press())   // Output: Windows Button Pressed
-	fmt.Println(checkbox.Check()) // Output: Windows Checkbox Checked
+}
 
-	var mf GUIFactory = &MacFactory{}
-	button = mf.CreateButton()
-	checkbox = mf.CreateCheckbox()
+func printShoeDetails(s IShoe) {
+	fmt.Printf("Logo: %s", s.getLogo())
+	fmt.Println()
+	fmt.Printf("Size: %d", s.getSize())
+	fmt.Println()
+}
 
-	fmt.Println(button.Press())   // Output: Mac Button Pressed
-	fmt.Println(checkbox.Check()) // Output: Mac Checkbox Checked
+func printShirtDetails(s IShirt) {
+	fmt.Printf("Logo: %s", s.getLogo())
+	fmt.Println()
+	fmt.Printf("Size: %d", s.getSize())
+	fmt.Println()
 }
