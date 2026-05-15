d4e3a1b49ee75016f6e9043fd0bf0e4b40444612
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2026-05-15 17:32:10 +0530

    add proxy pattern in Go

---
+++ pattern/proxy-pattern/application.go
@@ -0,0 +1,16 @@
+package main
+
+type Application struct {
+}
+
+func (a *Application) handleRequest(url, method string) (int, string) {
+	if url == "/app/status" && method == "GET" {
+		return 200, "ok"
+	}
+
+	if url == "/create/user" && method == "POST" {
+		return 201, "user created"
+	}
+
+	return 404, "Not ok"
+}
